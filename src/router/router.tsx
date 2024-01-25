e50402e6c91c29cbb6fea29a6fd3953f680fb23a
Jose Carvajal <jose@weightrx.com>
2024-01-25 10:24:56 -0500

    feat: adding permissions

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,49 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './router.interface';
 import {Home, ArtEvents, EventDetail, Favorites} from '../screens';
+import {PERMISSIONS, RESULTS, check, request} from 'react-native-permissions';
 
 const {Navigator, Screen} = createNativeStackNavigator<RootStackParamList>();
 
 export const Router: FC = () => {
+  useEffect(() => {
+    checkCalendarPermission();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const checkCalendarPermission = async () => {
+    try {
+      const statusCalendars = await check(PERMISSIONS.IOS.CALENDARS);
+      const statusWrite = await check(PERMISSIONS.IOS.CALENDARS_WRITE_ONLY);
+
+      if (
+        statusCalendars !== RESULTS.GRANTED ||
+        statusWrite !== RESULTS.GRANTED
+      ) {
+        requestCalendarPermission();
+      }
+    } catch (error) {
+      console.error('Error checking calendar permission:', error);
+    }
+  };
+
+  const requestCalendarPermission = async () => {
+    try {
+      const status = await request(PERMISSIONS.IOS.CALENDARS);
+      await request(PERMISSIONS.IOS.CALENDARS_WRITE_ONLY);
+
+      if (status === RESULTS.GRANTED) {
+        console.log('Calendar permission granted');
+      } else {
+        console.log('Calendar permission denied');
+      }
+    } catch (error) {
+      console.error('Error requesting calendar permission:', error);
+    }
+  };
+
   return (
     <NavigationContainer>
       <Navigator initialRouteName="Events">
