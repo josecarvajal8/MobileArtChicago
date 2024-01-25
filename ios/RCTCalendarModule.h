275da42c8c7380a747654967f94d239a94e5147c
Jose Carvajal <jose@weightrx.com>
2024-01-25 09:19:28 -0500

    feat: exposing method

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTCalendarModule.h
+//  MobileArtChicago
+//
+//  Created by Jose Carvajal on 25/01/24.
+//
+
+#ifndef RCTCalendarModule_h
+#define RCTCalendarModule_h
+
+
+#endif /* RCTCalendarModule_h */
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTCalendarModule.m
+//  MobileArtChicago
+//
+//  Created by Jose Carvajal on 25/01/24.
+//
+
+#import <Foundation/Foundation.h>
